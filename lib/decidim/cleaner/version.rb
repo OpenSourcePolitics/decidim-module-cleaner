9bebe11d11576614e37fa044e72d0d4adea107b8
armandfardeau <fardeauarmand@gmail.com>
2023-02-13 15:29:04 +0100

    Backport #6

--- lib/decidim/cleaner/version.rb
+++ lib/decidim/cleaner/version.rb
@@ -4,6 +4,10 @@ module Decidim
   # This holds the decidim-meetings version.
   module Cleaner
     def self.version
+      "0.0.1"
+    end
+
+    def self.decidim_version
       "0.26.4"
     end
   end
--- spec/lib/decidim/cleaner/version_spec.rb
+++ spec/lib/decidim/cleaner/version_spec.rb
@@ -9,5 +9,9 @@ module Decidim
     it "has version" do
       expect(subject.version).to eq("0.26.4")
     end
+
+    it "has decidim version" do
+      expect(subject.decidim_version).to eq("0.26.4")
+    end
   end
 end

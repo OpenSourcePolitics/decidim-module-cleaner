f0310641275871b544a2c9b37950660981b170cb
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-03-03 10:15:56 +0100

    clean module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 require "decidim/dev"
+require "simplecov"
+require "simplecov-cobertura"
+
+if ENV["CODECOV"]
+  SimpleCov.start "rails" do
+    add_filter "/spec/decidim_dummy_app/"
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter if ENV["CI"]
+end
 
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 

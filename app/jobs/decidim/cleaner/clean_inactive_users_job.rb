41006bdb747d4bc7cb14d31be95ef8b05c2f1790
armandfardeau <fardeauarmand@gmail.com>
2023-03-23 11:38:29 +0100

    Don't query deleted users

--- app/jobs/decidim/cleaner/clean_inactive_users_job.rb
+++ app/jobs/decidim/cleaner/clean_inactive_users_job.rb
@@ -10,17 +10,18 @@ module Decidim
           next unless organization.delete_inactive_users?
 
           send_warning(Decidim::User.where(organization: organization)
+                                    .not_deleted
                                     .where("last_sign_in_at < ?", Time.zone.now - (organization.delete_inactive_users_email_after || 365).days)
                                     .where("last_sign_in_at > ?", Time.zone.now - (organization.delete_inactive_users_email_after || 365).days - 1.day))
+
           delete_user_and_send_email(Decidim::User.where(organization: organization)
+                                                  .not_deleted
                                                   .where("last_sign_in_at < ?", Time.zone.now - (organization.delete_inactive_users_after || 390).days))
         end
       end
 
       def send_warning(users)
         users.find_each do |user|
-          next if user.deleted?
-
           InactiveUsersMailer.warning_inactive(user).deliver_now
           Rails.logger.info "Inactive warning sent to #{user.email}"
         end
@@ -28,8 +29,6 @@ module Decidim
 
       def delete_user_and_send_email(users)
         users.find_each do |user|
-          next if user.deleted?
-
           InactiveUsersMailer.warning_deletion(user).deliver_now
           Rails.logger.info "Deletion warning sent to #{user.email}"
 

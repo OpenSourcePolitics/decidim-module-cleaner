6a9a26799332bf95cd35cea86bcb88c44d1914fb
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-03-29 12:31:59 +0200

    refactor module, use warning timestamps

--- lib/decidim/cleaner.rb
+++ lib/decidim/cleaner.rb
@@ -7,5 +7,18 @@ require "decidim/cleaner/admin_engine"
 module Decidim
   # This namespace holds the logic of the `Cleaner` module.
   module Cleaner
+    include ActiveSupport::Configurable
+
+    config_accessor :delete_admin_logs_after do
+      365
+    end
+
+    config_accessor :delete_inactive_users_after do
+      30
+    end
+
+    config_accessor :delete_inactive_users_email_after do
+      365
+    end
   end
 end

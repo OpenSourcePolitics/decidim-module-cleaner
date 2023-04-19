70867b42484c22d58ecb695ad56fea1499ff5145
armandfardeau <fardeauarmand@gmail.com>
2023-04-19 17:49:08 +0200

    Fix migration

--- db/migrate/20230328094652_add_warning_date_to_users.rb
+++ db/migrate/20230328094652_add_warning_date_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddWarningDateToUsers < ActiveRecord::Migration[6.1]
+class AddWarningDateToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_users, :warning_date, :datetime
   end

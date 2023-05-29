a66c059a0189b3e2efd3b6e489f39cc572eeacfb
quentinchampenois <quentin.champenois7@gmail.com>
2023-05-29 12:09:18 +0200

    Update expected version in specs

--- spec/lib/decidim/cleaner/version_spec.rb
+++ spec/lib/decidim/cleaner/version_spec.rb
@@ -7,7 +7,7 @@ module Decidim
     subject { described_class }
 
     it "has version" do
-      expect(subject.version).to eq("0.1.0")
+      expect(subject.version).to eq("0.2.0")
     end
 
     it "has decidim version compatibility" do

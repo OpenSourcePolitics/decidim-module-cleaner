79622e39f56fe2098da07838d7341cf25c0341cc
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-11-16 14:32:10 +0100

    fix: Refactor 'destroy_user_account!' method in Decidim::DestroyAccount command

--- lib/decidim/cleaner/engine.rb
+++ lib/decidim/cleaner/engine.rb
@@ -8,6 +8,10 @@ module Decidim
     # This is the engine that runs on the public interface of cleaner.
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::Cleaner
+
+      config.to_prepare do
+        Decidim::DestroyAccount.include(Decidim::Cleaner::Extends::DestroyAccount)
+      end
     end
   end
 end
